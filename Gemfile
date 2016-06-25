bc16e746b7037c7c8dfadacdf65d92a02ca5cd8b
kgd <kylegraydev@gmail.com>
2016-06-24 19:15:28 -0700

    added postgres & initial table

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ gem 'rails', '4.0.2'
 gem 'bootstrap-sass', '3.3.0.0'
 gem "font-awesome-rails"
 
+gem 'pg'
+
 # Use sqlite3 as the database for Active Record
 
 # Use SCSS for stylesheets
---
+++ db/migrate/20160625020250_create_tax_sales.rb
@@ -0,0 +1,17 @@
+class CreateTaxSales < ActiveRecord::Migration
+  def change
+    create_table :tax_sales do |t|
+      t.string :parcel_num
+      t.string :name
+      t.string :address
+      t.string :legal_desc
+      t.string :min_bid
+      t.string :grid_num
+      t.string :amount_owed
+      t.binary :assesors_map
+      t.binary :aerial_image
+      t.binary :street_image
+      t.binary :defaulted_bill
+    end
+  end
+end
